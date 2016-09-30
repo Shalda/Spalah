4f9015e5ec51235824039371603fd23de79c01bf
Shalda <zeppelin.ua@gmail.com>
2016-09-30 12:22:41 +0300

    full_version

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        auto: true,
+        controls: false,
+        pause: 4000,            // время между сменой слайдов в м-сек
+        speed: 500,             // длительность перехода слайда в м-сек
+
+    });
+});
